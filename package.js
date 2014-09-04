768ef458cd56010ff28a0642802016bf568f249b
zimme <zimme@users.noreply.github.com>
2014-09-04 17:49:36 +0200

    Version bump
    Bump versions and update versions.json

--- package.js
+++ package.js
@@ -2,14 +2,14 @@ Package.describe({
   git: 'https://github.com/XpressiveCode/iron-router-active.git',
   name: 'zimme:iron-router-active',
   summary: 'An iron-router module for quickly adding classnames to your active navigation elements.',
-  version: '0.2.1'
+  version: '0.2.2-rc1'
 });
 
 Package.onUse(function(api){
-  api.versionsFrom('0.9.0');
+  api.versionsFrom('0.9.1-rc3');
   api.use([
     'ui',
-    'iron:router@0.9.0'
+    'iron:router@0.9.2-rc2'
   ], 'client');
 
   api.addFiles([
