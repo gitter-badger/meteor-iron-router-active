b9f008a57cbb8b252ba5cc73a8d10ef54288361d
zimme <zimme@users.noreply.github.com>
2014-09-11 13:45:05 +0200

    Version bump

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   git: 'https://github.com/zimme/meteor-iron-router-active.git',
   name: 'zimme:iron-router-active',
   summary: 'An iron-router module for quickly adding classnames to your active navigation elements.',
-  version: '0.2.2-rc1'
+  version: '0.2.2'
 });
 
 Package.onUse(function(api){
