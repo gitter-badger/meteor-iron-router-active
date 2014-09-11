b7337110659421ba949bb865bc4bcb789b145350
zimme <zimme@users.noreply.github.com>
2014-09-11 13:44:02 +0200

    Change git url

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  git: 'https://github.com/XpressiveCode/iron-router-active.git',
+  git: 'https://github.com/zimme/meteor-iron-router-active.git',
   name: 'zimme:iron-router-active',
   summary: 'An iron-router module for quickly adding classnames to your active navigation elements.',
   version: '0.2.2-rc1'
